a5321ec17c529939f67fca3ad84701b517f9f16a
Zestia <kevinbillerach@yahoo.fr>
2023-11-07 15:30:14 +0000

    config ssl

--- terraform-drupal/data.tf
+++ terraform-drupal/data.tf
@@ -19,3 +19,11 @@ data "aws_nat_gateway" "ngw" {
 data "aws_security_group" "jenkins-sg" {
   name = "kebi-jenkins-sg"
 }
+
+data "aws_route53_zone" "zone" {
+  name = "kevin-billerach.me"
+}
+
+data "aws_acm_certificate" "certificate" {
+  domain = "kevin-billerach.me"
+}
--- terraform-drupal/load-balancer.tf
+++ terraform-drupal/load-balancer.tf
@@ -27,13 +27,28 @@ resource "aws_lb_target_group_attachment" "instance-atc2" {
   target_id = aws_instance.webserver-2.id
 }
 
-resource "aws_lb_listener" "front_end" {
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.lb.arn
+  port = "443"
+  protocol = "HTTPS"
+  certificate_arn = data.aws_acm_certificate.certificate.arn
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.my-tg.arn
+  }
+}
+
+resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.lb.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.my-tg.arn
+    type             = "redirect"
+    redirect {
+      port = "443"
+      protocol = "HTTPS"
+      status_code = "HTTP_301"
+    }
   }
 }
