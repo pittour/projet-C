2e451bd9597baf4c3bbeab70bfdefc22be9b3e4e
Zestia <kevinbillerach@yahoo.fr>
2023-11-13 16:49:01 +0000

    ajout kube

--- terraform-ms/eks-cluster.tf
+++ terraform-ms/eks-cluster.tf
@@ -35,6 +35,16 @@ resource "aws_eks_node_group" "nodes" {
   }
 }
 
+# Si OIDC autorisé
+# data "tls_certificate" "eks" {
+#   url = aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+# }
+# resource "aws_iam_openid_connect_provider" "eks" {
+#   client_id_list = [ "sts.amazonaws.com" ]
+#   thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+#   url = aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+# }
+
 resource "kubernetes_service_account" "service-account" {
   metadata {
     name      = "aws-load-balancer-controller"
