6ae543be95e7929cf8da596e75217eb10860639c
Zestia <kevinbillerach@yahoo.fr>
2023-11-18 16:32:56 +0000

    fix deployment

--- terraform-ms/deployment.tf
+++ terraform-ms/deployment.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "deploy" {
   provisioner "local-exec" {
-    command = "aws eks update-kubeconfig --region eu-west-1 --name ${aws_eks_cluster.cluster.name} && kubectl apply -f ."
-    # command = "aws eks update-kubeconfig --region eu-west-1 --name ${aws_eks_cluster.cluster.name}"
+    # command = "aws eks update-kubeconfig --region eu-west-1 --name ${aws_eks_cluster.cluster.name} && kubectl apply -f ."
+    command = "aws eks update-kubeconfig --region eu-west-1 --name ${aws_eks_cluster.cluster.name}"
     working_dir = "../kubernetes-ms"
   }
 
